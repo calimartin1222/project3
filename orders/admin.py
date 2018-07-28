0ce5b33d535f2f0204ad0f18fd8b0530ae22977e
Cali-Ann Sara Martin <calimartin1222@gmail.com>
2018-07-28 03:11:31 +0000

    have order and menu database set up, just have to add items

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pizza, Sub, Pasta, Salad, Platter, subType, pastaType, saladType, platterType, pizzaType, topping
+
+# Register your models here.
+admin.site.register(Pizza)
+admin.site.register(Sub)
+admin.site.register(Pasta)
+admin.site.register(Salad)
+admin.site.register(Platter)
+
+admin.site.register(subType)
+admin.site.register(pastaType)
+admin.site.register(saladType)
+admin.site.register(platterType)
+admin.site.register(topping)
+admin.site.register(pizzaType)
+
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,84 @@
 from django.db import models
 
 # Create your models here.
+class Pizza(models.Model):
+    customer = models.CharField(max_length=64)
+    kind = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    toppings = models.CharField(max_length=64)
+    special = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.customer} ordered a {self.size}, {self.kind} Pizza with {self.toppings} - {self.special}."
+
+class Sub(models.Model):
+    customer = models.CharField(max_length=64)
+    kind = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    extras = models.CharField(max_length=64)
+    extraCheese = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.customer} ordered a {self.size}, {self.kind} Sub with {self.extras} - {self.extraCheese}."
+
+class Pasta(models.Model):
+    customer = models.CharField(max_length=64)
+    kind = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.customer} ordered a {self.kind} pasta."
+
+class Salad(models.Model):
+    customer = models.CharField(max_length=64)
+    kind = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.customer} ordered a {self.kind} salad."
+
+class Platter(models.Model):
+    customer = models.CharField(max_length=64)
+    kind = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.customer} ordered a {self.kind} Dinner Platter."
+#___________________________________________________________________________________________________________________________________
+class subType(models.Model):
+    type = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return f"{self.type} - ${self.price}"
+
+class pizzaType(models.Model):
+    type = models.CharField(max_length=64)
+    size = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return f"{self.type} - ${self.price}"
+
+class saladType(models.Model):
+    type = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+
+    def __str__(self):
+        return f"{self.type} - ${self.price}"
+
+class pastaType(models.Model):
+    type = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+    def __str__(self):
+        return f"{self.type} - ${self.price}"
+
+class platterType(models.Model):
+    type = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+    def __str__(self):
+        return f"{self.type} - ${self.price}"
+
+class topping(models.Model):
+    type = models.CharField(max_length=64)
+    price = models.DecimalField(decimal_places=2, max_digits=4)
+    def __str__(self):
+        return f"{self.type} - ${self.price}"
